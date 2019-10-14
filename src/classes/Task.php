97813b94cbc415d84ee79a4d8c522ad9f17d8439
Molchanov Ilia <plue@inbox.ru>
2019-10-14 20:00:07 +0300

    статусы и действия

--- src/classes/Task.php
+++ src/classes/Task.php
@@ -8,37 +8,77 @@
 
 class Task
 {
-    private $statuses = ['Новое', 'Выполняется', 'Завершено', 'Отменено'];
-    private $roles = [];
-    private $operations = [];
 
-    private $currentStatus = null;
-    private $ownerId = null;
-    private $workerId = null;
+    const STATUS_NEW = 'Новое';
+    const STATUS_IN_WORK = 'Выполняется';
+    const STATUS_COMPLETED = 'Завершено';
+    const STATUS_CANCEL = 'Отменено';
 
-    public function __construct($status = 'Новое')
-    {
-        $this->currentStatus = $status;
+    protected $currentStatus;
+
+    function __construct(){
+
+        $this->begin();
+
     }
 
-    public function getStatuses()
-    {
-        return $this->statuses;
-    }
+    private $map = [
+        self::STATUS_NEW => [self::STATUS_IN_WORK, self::STATUS_CANCEL],
+        self::STATUS_IN_WORK => [self::STATUS_COMPLETED, self::STATUS_CANCEL],
+        self::STATUS_COMPLETED => null,
+        self::STATUS_CANCEL => null,
+    ];
 
-    public function getRoles()
-    {
-        return $this->roles;
-    }
 
-    public function getCurrentStatus()
+
+    public function getStatus()
     {
         return $this->currentStatus;
     }
 
-    public function getNextStatus()
+    public function canChange($newStatus)
     {
-        return $this->roles;
+
+        $current = $this->getStatus();
+
+        // при создании, когда еще нет статуса
+        if (!$current && $newStatus === self::STATUS_NEW) {
+            return true;
+        }
+
+        $nextStatuses = $this->map[$current];
+
+        return (!empty($nextStatuses) && in_array($newStatus, $nextStatuses));
+    }
+
+
+    public function setStatus($newStatus)
+    {
+
+        if ($this->canChange($newStatus)) {
+
+            $this->currentStatus = $newStatus;
+            return true;
+        }
+
+        echo 'Ошибка';
+        return false;
+    }
+
+    public function begin() {
+        $this->setStatus(self::STATUS_NEW);
+    }
+
+    public function cancel() {
+        $this->setStatus(self::STATUS_CANCEL);
+    }
+
+    public function start() {
+        $this->setStatus(self::STATUS_IN_WORK);
+    }
+
+    public function complete() {
+        $this->setStatus(self::STATUS_COMPLETED);
     }
 
 }
