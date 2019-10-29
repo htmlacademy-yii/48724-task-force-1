896448b7a7d1fa644c2020e6dde2bd66a3525375
Ilia <plue@inbox.ru>
2019-10-29 09:10:10 +0300

    проектирование процессов

---
+++ src/models/UserNotification.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: plue
+ * Date: 27/10/2019
+ * Time: 20:51
+ */
+
+namespace App\models;
+
+
+class UserNotification
+{
+
+    private $id;
+    private $user_id;
+    private $new_message;
+    private $task_action;
+    private $new_review;
+
+    public function validate()
+    {
+
+    }
+
+    public function loadData($data)
+    {
+
+    }
+
+    public function save()
+    {
+
+    }
+
+}
